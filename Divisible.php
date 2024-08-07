8ab08e43bb0068b91bd92f12449173cd6c2bc3e0
SelvaChrista <selvachrista@gmail.com>
2024-08-07 21:20:07 +0530

    Add files via upload

---
+++ Divisible.php
@@ -0,0 +1,27 @@
+<?php
+
+
+$n = 17; // replace 50 with the number you want to check
+
+
+if($n % 3 == 0) {
+
+ echo "DIVISIBLE BY 3";
+
+} else {
+
+ if($n % 2 == 0) {
+
+   echo "EVEN NUMBER";
+
+ } else {
+
+   echo "ODD NUMBER";
+
+ }
+
+}
+
+
+?>
+
